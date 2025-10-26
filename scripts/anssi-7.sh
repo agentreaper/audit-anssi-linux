68b82bfc646a4db6217d7924284aa46294477f84
ebertinbench <agenthunk27@gmail.com>
2025-10-26 19:00:06 +0100

    refonte du script anssi-7.sh les scripts individuels renverront maintenant un code pour marquer le succès ou l'échec

--- scripts/anssi-7.sh
+++ scripts/anssi-7.sh
@@ -1,15 +1,14 @@
-function checkIOMMU(){
-    if cat /proc/cmdline | grep -q 'intel_iommu=on' || \
-       cat /proc/cmdline | grep -q 'amd_iommu=force_isolation' || \
-       cat /proc/cmdline | grep -q 'iommu=pt'; then
-        return 0
-    else
-        return 1
-    fi
+#!/bin/bash
+check_IOMMU() {
+    grep -qE '\b(intel_iommu=on|amd_iommu=force_isolation|iommu=pt)\b' /proc/cmdline
 }
 
-if checkIOMMU; then
-    echo "IOMMU is enabled"
+if check_IOMMU; then
+    echo "IOMMU est activé" >&2
+    exit 0
 else
-    echo "IOMMU is not enabled"
+    echo "IOMMU n'est pas activé" >&2
+    exit 1
 fi
+# Pour obtenir le code de sortie :
+# ./scripts/anssi-7.sh > /dev/null 2>&1; echo $?
