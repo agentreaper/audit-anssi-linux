0c415322d9e6558b713d17d1dfdd80122964c903
ebertinbench <agenthunk27@gmail.com>
2025-10-26 19:03:39 +0100

    ajout d'une fonction pour vérifier les en-tête du CSV et d'une fonction pour rajouter une ligne au csv souhaité

--- traitement_csv.sh
+++ traitement_csv.sh
@@ -1,9 +1,30 @@
+#!/bin/bash
+function check_headers(){
+    IFS=',' read -a var < "$1"
+    expected_headers=("id" "nom" "description" "niveau")
+    for i in "${!expected_headers[@]}"; do
+        if [ "${var[i]}" != "${expected_headers[i]}" ]; then
+            echo "headers incorrects sur la colonne $((i+1)): souhaité '${expected_headers[i]}', obtenu '${var[i]}'" >&2
+            return 1
+        fi
+    done
+    echo "Les headers sont corrects."
+    return 0
+}
 
-while IFS="," read -r rec_column1 rec_column2 rec_column3 rec_column4
-do
-  echo "value column 1: $rec_column1"
-  echo "value column 2: $rec_column2"
-  echo "value column 3: $rec_column3"
-  echo "value column 4: $rec_column4"
-  echo ""
-done < <(tail -n +2 test.csv)
+function ajouter_recommandation(){
+    rec_id="$1"
+    rec_nom="$2"
+    rec_description="$3"
+    rec_niveau="$4"
+    echo "$rec_id,$rec_nom,$rec_description,$rec_niveau" >> "$5"
+}
+# while IFS="," read -r rec_column1 rec_column2 rec_column3 rec_column4
+# do
+#   echo "value column 1: $rec_column1"
+#   echo "value column 2: $rec_column2"
+#   echo "value column 3: $rec_column3"
+#   echo "value column 4: $rec_column4"
+#   echo ""
+# done < <(tail -n +2 test.csv)
+check_headers "$1"
