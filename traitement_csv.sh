bcdc378a488a3b4a0f309899af0d4aa630b5d279
ebertinbench <ebertinbench@vbox.wireless.thales>
2025-10-27 13:54:52 +0100

    ajout d'une fonction pour corriger les headers et d'une vérification que le fichier csv existe. Si il n'existe pas encore alors il est créé

--- traitement_csv.sh
+++ traitement_csv.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 function check_headers(){
+    if [ -f "$1" ]; then
+        echo "le fichier csv n'existe pas encore et va être créé"
+        touch "$1"
+    fi
     IFS=';' read -a var < "$1"
     expected_headers=("id" "nom" "description" "niveau" "succes" "commentaire")
     for i in "${!expected_headers[@]}"; do
         if [ "${var[i]}" != "${expected_headers[i]}" ]; then
             echo "headers incorrects sur la colonne $((i+1)): souhaité '${expected_headers[i]}', obtenu '${var[i]}'" >&2
-            return 1
+            remake_header "$1" || return 1
+            return 0
         fi
     done
     echo "Les headers sont corrects."
@@ -13,11 +18,11 @@ function check_headers(){
 }
 
 function ajouter_recommandation(){
+    check_headers "$5"
     jq -r '.[] | @base64' "$1" | while IFS= read -r row; do
         rowformatted=$(printf '%s' "$row" | base64 --decode)
         id=$(printf '%s' "$rowformatted" | jq -r '.id')
         if [ $id = "$2" ]; then
-            echo "la recommandation $id existe"
             commentaire="$3"
             succes="$4"
             nom=$(printf '%s' "$rowformatted" | jq -r '.nom')
@@ -26,6 +31,10 @@ function ajouter_recommandation(){
             echo -e "$id;$nom;$description;$niveau;$succes;$commentaire" >> "$5"
         fi
     done
+}
+function remake_header(){
+    header="id;nom;description;niveau;succes;commentaire"
+    sed -i "1s/.*/$header/" "$1"
 }
 # while IFS="," read -r rec_column1 rec_column2 rec_column3 rec_column4
 # do
